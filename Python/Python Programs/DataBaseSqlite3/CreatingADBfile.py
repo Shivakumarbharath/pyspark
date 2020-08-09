7642571b36d0955bee938e672879419ec9100297
Shivakumarbharath <shivakumar.bharath@gmail.com>
2020-08-09 20:45:56 +0530

    Assingment_2
    Oops concepts

---
+++ Python/Python Programs/DataBaseSqlite3/CreatingADBfile.py
@@ -0,0 +1,7 @@
+import sqlite3
+
+conn = sqlite3.connect("Our_data.db")  # open a database
+# .db,.sqlite3,.dbase
+
+
+conn.close()
--- Python/PythonAdvanced/FileHandling/Open file.py
+++
@@ -1,3 +0,0 @@
-fp=open(r'test.txt','w')#here r means raw which prevents the use of \n \t etc
-print(fp)
-fp.close()
